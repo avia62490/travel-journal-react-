9f77f700c5aa44dc4923ea647c341d82382ba894
Alex Via <alexandervia@gmail.com>
2023-01-02 13:12:15 -0600

    creates header, adds default data file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
+import data from './data.js';
+import globe from './globeIcon.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={globe} className="globeIcon" alt="globe" />
+        <h4>Travel Journal</h4>
       </header>
     </div>
   );
