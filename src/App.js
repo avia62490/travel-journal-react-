da9e1f08e337f7a7beffb8a4df16b866c48332da
Alex Via <alexandervia@gmail.com>
2023-01-02 13:37:25 -0600

    adds card component, passing props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
 import data from './data.js';
 import globe from './globeIcon.svg'
+import Card from './Card.js'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={globe} className="globeIcon" alt="globe" />
-        <h4>Travel Journal</h4>
-      </header>
-    </div>
-  );
+    const cards = data.map(item => {
+      return(
+        <Card 
+            key={item.id}
+            item={item}
+        />
+      )
+    })
+    return (
+        <div className="App">
+            <header className="App-header">
+              <img src={globe} className="globeIcon" alt="globe" />
+              <h4>Travel Journal</h4>
+            </header>
+            <section className="cards_section">
+                {cards}
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,5 @@
+export default function Card(props) {
+  return (
+    <img src={props.item.image} className="card--image" alt="" />
+  )
+}
