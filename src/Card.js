72cdc91076addabacd4b034cda166a9efe51ca8e
Alex Via <alexandervia@gmail.com>
2023-01-03 12:41:46 -0600

    passes google map url link

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,11 @@
+import marker from './markerIcon.png'
+
 export default function Card(props) {
-  return (
-    <img src={props.item.image} className="card--image" alt="" />
-  )
+    return (
+        <section className="card">
+            <img src={props.item.image} className="card--image" alt="" />
+            <img src={marker} className="card-markerIcon"  alt="" />
+            <h4><a href={props.item.googleMapsUrl}>View on google maps</a></h4>
+        </section>
+    )
 }
