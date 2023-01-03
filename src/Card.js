7f6c3c4a0c218353b3724922191f1d9d7233b811
Alex Via <alexandervia@gmail.com>
2023-01-03 12:53:18 -0600

    passes all data points through Card props

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,11 @@ export default function Card(props) {
         <section className="card">
             <img src={props.item.image} className="card--image" alt="" />
             <img src={marker} className="card-markerIcon"  alt="" />
+            <h3>{props.item.location}</h3>
             <h4><a href={props.item.googleMapsUrl}>View on google maps</a></h4>
+            <h1>{props.item.title}</h1>
+            <h4>{props.item.startDate} - {props.item.endDate}</h4>
+            <p>{props.item.description}</p>
         </section>
     )
 }
